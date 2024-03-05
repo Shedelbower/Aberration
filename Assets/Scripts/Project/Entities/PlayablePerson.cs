40e13fcdcc0df6c57eb162724e646150f48e8128
Alec Shedelbower <alec.shedelbower@gmail.com>
2024-03-04 20:35:07 -0800

    Switched from the CharacterController to manually adjusting a Rigidbody
    - Encountered some weird teleporting errors with the CharacterController

--- Assets/Scripts/Project/Entities/PlayablePerson.cs
+++ Assets/Scripts/Project/Entities/PlayablePerson.cs
@@ -7,8 +7,8 @@ namespace Project.Entities
 {
     public class PlayablePerson : PlayableEntity
     {
-        public Vector3 MovementForward => this.transform.forward;
-        public Vector3 MovementRight => this.transform.right;
+        public Vector3 MovementForward => _yawBase.forward;
+        public Vector3 MovementRight => _yawBase.right;
         public Vector3 RotationAxis => this.transform.up;
 
 
@@ -22,7 +22,15 @@ namespace Project.Entities
         [SerializeField] private float _maxVerticalAngleDegress = 70f;
         
         [Header("Component References")]
-        [SerializeField] private CharacterController _controller;
+        [SerializeField] private Rigidbody _rb;
+        
+        [SerializeField] private Transform _yawBase;
+        [SerializeField] private Transform _pitchBase;
+        
+        [Header("Debug")]
+        [SerializeField] private bool _isGrounded;
+        
+        private float _desiredHeadHeight = 1.75f;
 
         public override void OnActivated()
         {
@@ -36,36 +44,20 @@ namespace Project.Entities
             InputManager.Instance.UnlockMouse();
         }
 
-
         private void Update()
         {
-            if (!this.IsActive) { return; }
-
-            HandleMovement();
-        }
-
-        private void HandleMovement()
-        {
-            // Movement
-            var forwardInput = InputManager.Instance.VerticalAxis;
-            var rightInput = InputManager.Instance.HorizontalAxis;
-
-            var moveDir = forwardInput * this.MovementForward + rightInput * this.MovementRight;
-            moveDir = moveDir.normalized;
-            var speed = InputManager.Instance.ShiftKey ? _sprintSpeed : _walkSpeed;
-
-            _controller.SimpleMove(moveDir * speed * Time.deltaTime);
-            
-            // Rotation
+            // Rotation (Yaw)
             float mouseX = InputManager.Instance.MouseX;
             float angleX = mouseX * Time.deltaTime * _mouseSensitivityHorizontal;
             var rotX = Quaternion.AngleAxis(angleX, this.RotationAxis);
 
-            this.transform.rotation = rotX * this.transform.rotation;
+            _yawBase.rotation = rotX * _yawBase.rotation;
+            
+            // Rotation (Pitch)
             
             float mouseY = InputManager.Instance.MouseY;
             float verticalDelta = -mouseY * _mouseSensitivityVertical * Time.deltaTime;
-            float verticalAngle = _camera.transform.localEulerAngles.x;
+            float verticalAngle = _pitchBase.localEulerAngles.x;
 
             while (verticalAngle > 90f)
             {
@@ -73,7 +65,74 @@ namespace Project.Entities
             }
             
             verticalAngle = Mathf.Clamp(verticalAngle + verticalDelta, -_minVerticalAngleDegrees, _maxVerticalAngleDegress);
-            _camera.transform.localRotation = Quaternion.Euler(verticalAngle, 0, 0);
+            _pitchBase.localRotation = Quaternion.Euler(verticalAngle, 0, 0);
+        }
+
+
+        private void FixedUpdate()
+        {
+            if (!this.IsActive) { return; }
+
+            GroundCheckAndGravity();
+            HandleMovement();
+        }
+
+        private void GroundCheckAndGravity()
+        {
+            float bufferLength = 0.1f;
+            float castLength = bufferLength + _desiredHeadHeight;
+            var mask = LayerMask.GetMask("Walkable");
+
+            float gravityScale = 1.0f;
+            var currHeight = 10000f;
+            
+            if (Physics.Raycast(_pitchBase.position, Vector3.down, out RaycastHit hit, castLength, mask, QueryTriggerInteraction.Ignore))
+            {
+                currHeight = hit.distance;
+            }
+            
+            if (currHeight > _desiredHeadHeight)
+            {
+                _isGrounded = false;
+                float falloff = (currHeight - _desiredHeadHeight) / bufferLength;
+                falloff = Mathf.Clamp01(falloff);
+                gravityScale = falloff;
+            }
+            else
+            {
+                _isGrounded = true;
+                var verticalDelta = _desiredHeadHeight - currHeight;
+                _rb.MovePosition(_rb.position + Vector3.up * verticalDelta);
+                var vel = _rb.velocity;
+                vel.y = 0.0f;
+                _rb.velocity = vel;
+            }
+
+            if (!_isGrounded)
+            {
+                // Gravity
+                _rb.AddForce(Vector3.up * -9.8f * gravityScale, ForceMode.Acceleration); 
+            }
+        }
+
+        private void HandleMovement()
+        {
+            if (_isGrounded)
+            {
+                // Movement
+                var forwardInput = InputManager.Instance.VerticalAxis;
+                var rightInput = InputManager.Instance.HorizontalAxis;
+
+                var moveDir = forwardInput * this.MovementForward + rightInput * this.MovementRight;
+                moveDir = moveDir.normalized;
+                var speed = InputManager.Instance.ShiftKey ? _sprintSpeed : _walkSpeed;
+
+                var newHorizontalVelocity = moveDir * speed;
+                newHorizontalVelocity.y = _rb.velocity.y; // Preserve vertical velocity
+
+                _rb.velocity = newHorizontalVelocity;
+            }
+            
         }
 
     }
